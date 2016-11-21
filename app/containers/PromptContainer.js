a2cc7dc7372ade608496e4412194779473c45ff8
Eric Fries <ericfries@gmail.com>
2016-11-21 09:51:31 -0500

    finish crazy long video

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,31 +1,57 @@
 var React = require('react');
-var transparentBg = require('../styles').transparentBg;
+var Prompt = require('../components/Prompt');
 
 var PromptContainer = React.createClass({
+  // use only for routers
+  // this way router doens't have to be passed down as props to all components
+    contextTypes: {
+      router: React.PropTypes.object.isRequired
+    },
+
+    getInitialState: function() {
+      console.log(this.context);
+      return {
+        username: ''
+      }
+    },
+
+    handleUpdateUser: function(e) {
+      this.setState({
+        username: e.target.value
+      })
+    },
+
+    handleSubmitUser: function (e){
+      e.preventDefault();
+      var username = this.state.username;
+      this.setState({
+        username: '',
+      });
+
+      //calling push on route activates the routes
+      if (this.props.routeParams.playerOne){
+        this.context.router.push({
+          pathname: '/battle',
+          query: {
+            playerOne: this.props.routeParams.playerOne,
+            playerTwo: this.state.username
+          }
+        })
+        // go to /battle
+      } else {
+        this.context.router.push('playerTwo/' + this.state.username)
+        // go to/PlayerTwo
+      }
+    },
+
     render: function () {
-      console.log(this);
-        return (
-            <div className="jumbtotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-              <h1>{this.props.route.header}</h1>
-              <div className="col-sm-12">
-                <form>
-                  <div className="form-group">
-                    <input
-                      className="form-control"
-                      placeholder="Github Username"
-                      type="text" />
-                  </div>
-                  <div className="form-group col-sm-4 col-sm-offset-4">
-                    <button
-                      className="btn btn-block btn-success"
-                      type="submit">
-                      Continue
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-        )
+      return (
+        <Prompt
+          onSubmitUser={this.handleSubmitUser}
+          onUpdateUser={this.handleUpdateUser}
+          header={this.props.route.header}
+          username={this.state.username} />
+      )
     }
 });
 
